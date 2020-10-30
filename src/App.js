ded22ab021ad131fe9ff50833544e68b64a8cf1a
iagoscippo <iagoscippo@gmail.com>
2020-10-30 12:05:26 -0300

    handleAdd ya no interfiere

--- src/App.js
+++ src/App.js
@@ -5,49 +5,45 @@ import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
 
+  let available = 15;
+  let initial = 1;
 
-let available = 15;
-let initial = 1;
+  let [stock, setStock] = useState(available);
+  let [cart, setCart] = useState(0);
 
+  /* useEffect(() => {
+    console.log("Initialized");
+  }, []); */
 
-let [counter, setCounter] = useState(initial);
-let [stock, setStock] = useState(available);
-let [cart, setCart] = useState(0);
-
-/*
-useEffect(() => {
-  console.log("Initialized");
-}, []); */
-
- const  handleAdd = ()=>{
-  if (stock > 0) {
-    let resto = stock - counter;
-    setCart(counter + cart);
-    setStock(resto);
-    setCounter( (initial > resto) ? resto : initial); //opcional 
-  } else {
-    alert('no queda stock :(');
+  const  handleAdd = ()=>{
+  /* if (stock > 0) {
+      let resto = stock - counter;
+      setCart(counter + cart);
+      setStock(resto);
+      setCounter( (initial > resto) ? resto : initial); //opcional 
+    } else {
+      alert('no queda stock :(');
+    } */
   }
- }
 
- const info = {
-  width: '100%',
-  marginTop: 50,
-  textAlign: 'center'
- }
+  const info = {
+    width: '100%',
+    marginTop: 50,
+    textAlign: 'center'
+  }
 
-let itemCountProps = {stock, initial, handleAdd, counter};
+  let itemCountProps = {stock, initial, handleAdd};
 
- return (
-  <div className="App">
-    <NavBar />
-      <div className="info" style={info}>
-        <ItemCount {...itemCountProps}/>
-        <h3>carrito: {cart}</h3>
-        <ItemListContainer title="FURIA PRINT"/>
+  return (
+    <div className="App">
+      <NavBar />
+        <div className="info" style={info}>
+          <ItemCount {...itemCountProps}/>
+          <h3>carrito: {cart}</h3>
+          <ItemListContainer title="FURIA PRINT"/>
+      </div>
     </div>
-  </div>
-)
-};
+  )
+  };
 
 export default App;
